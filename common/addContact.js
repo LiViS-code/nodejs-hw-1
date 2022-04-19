2374b490950a7f825762d1e30f4fa4ab7bb56197
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-04-19 21:42:42 +0300

    change generator id

--- common/addContact.js
+++ common/addContact.js
@@ -1,12 +1,12 @@
 const fs = require("fs/promises");
-const { v4 } = require("uuid");
+const { nanoid } = require("nanoid");
 const listContacts = require("./listContacts");
 const pathFile = require("./pathFile");
 
 async function addContact({ name, email, phone }) {
   try {
     const list = await listContacts();
-    const newContact = { name, email, phone, id: v4() };
+    const newContact = { name, email, phone, id: nanoid() };
     list.push(newContact);
     await fs.writeFile(pathFile, JSON.stringify(list));
     return newContact;
