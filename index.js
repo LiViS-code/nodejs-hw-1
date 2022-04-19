5db1de53033053d1552013954ae1d79d969b834b
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-04-19 12:03:12 +0300

    upd

--- index.js
+++ index.js
@@ -2,36 +2,40 @@ const contactsOperations = require("./contacts");
 const argv = require("yargs").argv;
 
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      const contacts = await contactsOperations.listContacts();
-      console.table(contacts);
-      break;
+  try {
+    switch (action) {
+      case "list":
+        const contacts = await contactsOperations.listContacts();
+        console.table(contacts);
+        break;
 
-    case "get":
-      const contact = await contactsOperations.getContactById(id);
-      if (!contact) {
-        throw new Error(`contact with id ${id} not found`);
-      }
-      console.log(contact);
-      break;
+      case "get":
+        const contact = await contactsOperations.getContactById(id);
+        if (!contact) {
+          throw new Error(`contact with id ${id} not found`);
+        }
+        console.log(contact);
+        break;
 
-    case "add":
-      const newContact = await contactsOperations.addContact({
-        name,
-        email,
-        phone,
-      });
-      console.log(newContact);
-      break;
+      case "add":
+        const newContact = await contactsOperations.addContact({
+          name,
+          email,
+          phone,
+        });
+        console.log(newContact);
+        break;
 
-    case "remove":
-      const removeContact = await contactsOperations.removeContact(id);
-      console.log(removeContact);
-      break;
+      case "remove":
+        const removeContact = await contactsOperations.removeContact(id);
+        console.log(removeContact);
+        break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (error) {
+    console.log(error.message);
   }
 }
 
