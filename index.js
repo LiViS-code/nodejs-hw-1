ab30a375a1a24521c1ab3482f4340a1d20525d56
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-04-19 12:34:51 +0300

    upd

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
-const contactsOperations = require("./contacts");
 const argv = require("yargs").argv;
 
 async function invokeAction({ action, id, name, email, phone }) {
-  try {
-    switch (action) {
-      case "list":
-        const contacts = await contactsOperations.listContacts();
-        console.table(contacts);
-        break;
+  switch (action) {
+    case "list":
+      const contacts = await contactsOperations.listContacts();
+      console.table(contacts);
+      break;
 
-      case "get":
-        const contact = await contactsOperations.getContactById(id);
-        if (!contact) {
-          throw new Error(`contact with id ${id} not found`);
-        }
-        console.log(contact);
-        break;
+    case "get":
+      const contact = await contactsOperations.getContactById(id);
+      if (!contact) {
+        throw new Error(`contact with id ${id} not found`);
+      }
+      console.log(contact);
+      break;
 
-      case "add":
-        const newContact = await contactsOperations.addContact({
-          name,
-          email,
-          phone,
-        });
-        console.log(newContact);
-        break;
+    case "add":
+      const newContact = await contactsOperations.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.log(newContact);
+      break;
 
-      case "remove":
-        const removeContact = await contactsOperations.removeContact(id);
-        console.log(removeContact);
-        break;
+    case "remove":
+      const removeContact = await contactsOperations.removeContact(id);
+      console.log(removeContact);
+      break;
 
-      default:
-        console.warn("\x1B[31m Unknown action type!");
-    }
-  } catch (error) {
-    console.log(error.message);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
 }
 
-invokeAction(argv);
+(async () => {
+  await invokeAction(argv);
+})();
